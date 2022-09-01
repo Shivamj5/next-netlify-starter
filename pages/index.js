f9400ee9519c1d27a00af5920cbd83905e93769b
Shivamj5 <66289349+Shivamj5@users.noreply.github.com>
2022-09-01 07:36:17 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,11 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+    
+   <h1>MyAnimeGuru</h1>
+<p>My first paragraph.</p>
+    <a href="https://www.myanimeguru.com">MyAnimeGuru</a>
+
         </p>
       </main>
 
